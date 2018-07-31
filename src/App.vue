769f1d08469fc9646963712bb5d49e3026ef1f76
Yuriy Hvozdovych <gvizdyk@quintagroup.com>
2018-07-31 03:42:42 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  html {
+    height: 100%;
+    width: 100%;
+  }
+  @media (max-width: 767px) {
+    html {
+      font-size: 14px;
+    }
+  }
+  body {
+    align-content: center;
+    align-items: center;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+    height: 100%;
+    justify-content: center;
+    margin: 0;
+    padding: 0;
+    width: 100%;
+  }
+  h1 {
+    font-size: 6.250rem;
+    margin: 0;
+  }
+  a {
+    color: #000;
+    text-decoration: underline;
+  }
+</style>
---
+++ src/components/FrontPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>#OGG</h1>
+    <p><a href="mailto:ogg.design.team@gmail.com">ogg.design.team@gmail.com</a></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FrontPage',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+  p {
+    text-align: center;
+  }
+</style>
