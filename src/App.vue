0bad0fa4cc0f0f4c371c3921051595d24000e299
Yuriy Hvozdovych <gvizdyk@quintagroup.com>
2018-08-08 03:16:13 +0300

    updated styles

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,16 @@ export default {
 </script>
 
 <style>
+  @font-face {
+      font-family: 'poppinssemibold';
+      src: url('./assets/fonts/poppins-semibold-webfont.eot');
+      src: url('./assets/fonts/poppins-semibold-webfont.eot?#iefix') format('embedded-opentype'),
+            url('./assets/fonts/poppins-semibold-webfont.woff2') format('woff2'),
+            url('./assets/fonts/poppins-semibold-webfont.woff') format('woff'),
+            url('./assets/fonts/poppins-semibold-webfont.svg#poppinssemibold') format('svg');
+      font-weight: normal;
+      font-style: normal;
+  }
   html {
     height: 100%;
     width: 100%;
@@ -21,12 +31,12 @@ export default {
     }
   }
   body {
+    font-family: 'poppinssemibold', sans-serif;
     align-content: center;
     align-items: center;
     box-sizing: border-box;
     display: flex;
     flex-direction: column;
-    font-family: 'Lato', sans-serif;
     height: 100%;
     justify-content: center;
     margin: 0;
@@ -36,11 +46,15 @@ export default {
     color: #fff;
   }
   h1 {
-    font-size: 6.250rem;
+    font-size: 5rem;
     margin: 0;
   }
   a {
     color: #000;
     text-decoration: underline;
+  }
+  #app {
+    height: 100%;
+    width: 100%;
   }
 </style>
--- src/components/FrontPage.vue
+++ src/components/FrontPage.vue
@@ -2,29 +2,25 @@
   <div class="wrapper">
     <section class="slider slider-1">
       <div class="dots">
-        <div class="dots-wrapper">
-          <div class="dots-items">
-            <div class="dots-item">
-              <div class="dots-item-button dots-item-button--orange">
-              </div>
-            </div>
-            <div class="dots-item">
-              <div class="dots-item-button dots-item-button--pink">
-              </div>
-            </div>
-            <div class="dots-item">
-              <div class="dots-item-button dots-item-button--blue">
-              </div>
-            </div>
-          </div>
-        </div>
+        <div class="dots__item"></div>
+        <div class="dots__item"></div>
+        <div class="dots__item"></div>
       </div>
+      <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+        <defs>
+          <filter id="goo">
+            <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="goo" />
+            <feComposite in="SourceGraphic" in2="goo" operator="atop"/>
+          </filter>
+        </defs>
+      </svg>
     </section>
     <section class="slider slider-2">
-      <h1>Yes, just f*cking 3 dots!</h1>
+      <h1>Yes, just f&#9880;cking 3 dots!</h1>
     </section>
     <section class="slider slider-3">
-      <h1>Bacause that's simple & effective</h1>
+      <h1>Because that's simple &#38; effective</h1>
     </section>
     <section class="slider slider-4">
       <h1>Who we are?</h1>
@@ -48,13 +44,11 @@ export default {
 
 
 <style scoped>
-  #app,
   .wrapper {
     height: 100%;
     width: 100%;
   }
   .slider {
-    /* animation:  1s infinite ease-in-out; */
     display: flex;
     flex-direction: row;
     flex-wrap: nowrap;
@@ -70,54 +64,76 @@ export default {
     left: 0;
     right: 0;
     transform: translateX(100%);
-    animation-duration: 3s;
+    animation-duration: 4s;
   }
   .slider-1 {
     transform: translateX(0);
-    animation-name: fadeIn1;
+    /* animation-name: fadeIn1;
     animation-duration: 3s;
     animation-delay: 2s;
-    animation-fill-mode: forwards;
+    animation-fill-mode: forwards; */
   }
   .slider-2 {
     opacity: 0;
     transform: translateX(0);
     animation-delay: 5s;
-    animation-name: fadeIn;
+    animation-name: fadeIn-sl2;
   }
-  .slider-3 {
-    animation-delay: 8s;
-    animation-name: wobble;
-  }
-  .slider-4 {
-    animation-delay: 11s;
-    animation-name: wobble;
-  }
-  .slider-5 {
-    /* transform: translateX(0); */
-    animation-delay: 13s;
-    animation-duration: 7s;
-    /* animation-name: wobble-1, wobble-2, wobble-3; */
-    animation-name: wobble-1;
-  }
-  @keyframes wobble-first {
-    0% {
+  @keyframes fadeIn-sl2 {
+    0% {opacity: 0;}
+    50% {
+      opacity: 1;
       transform: translateX(0);
     }
     100% {
+      opacity: 1;
       transform: translateX(-100%);
     }
   }
-  @keyframes wobble {
+
+  .slider-3 {
+    animation-delay: 7s;
+    animation-name: wobble121;
+    animation-duration: 5s;
+  }
+  @keyframes wobble121 {
     0% {
       transform: translateX(100);
     }
-    50% {
+    40%,60% {
       transform: translateX(0);
     }
     100% {
       transform: translateX(-100%);
     }
+  }
+
+
+
+
+
+  .slider-4 {
+    animation-delay: 10s;
+    animation-name: wobble-sl4;
+  }
+  @keyframes wobble-sl4 {
+    0% {
+      transform: translateX(100);
+    }
+    40%,60% {
+      transform: translateX(0);
+    }
+    100% {
+      transform: translateX(-100%);
+    }
+  }
+
+
+
+  .slider-5 {
+    animation-delay: 12.4s;
+    animation-duration: 7s;
+    animation-name: wobble-1;
   }
   @keyframes wobble-1 {
     0% {
@@ -130,11 +146,15 @@ export default {
       transform: translateX(-100%);
     }
   }
+
+
+
+
   @keyframes fadeIn1 {
     0%, 50% {opacity: 0;}
     100% {opacity: 1;}
   }
- @keyframes fadeIn {
+  @keyframes fadeIn {
     0% {opacity: 0;}
     50% {opacity: 1;}
     100% {opacity: 0;}
@@ -142,72 +162,56 @@ export default {
 
 
 
-.dots-wrapper {
-	transform: translate(-50%, -50%);
+
+
+
+
+
+.dots {
+  filter: url("#goo");
   position: absolute;
-  top: 50%;
-      animation-duration: 7s;
-    animation-name: wobble-trt;
-    animation-fill-mode: forwards;
+  top: calc(50% - 18px);
+  left: calc(50% - 115px);
+  animation-duration: 7s, 2s;
+  animation-delay: 0.1s, 16.7s;
+  animation-name: dots-move-top, dots-move-left;
+  animation-fill-mode: forwards;
 }
- @keyframes wobble-trt {
-    0%,75% {top: 50%;}
-    100% {top:35px;}
-  }
-
-
-
-.dots-item-button {
-	border-radius: 50%;
+@keyframes dots-move-top {
+  0%,65% {top:calc(50% - 18px);}
+  100% {top:35px;}
 }
-
-
-.dots-items {
-  transition-timing-function: cubic-bezier(0.64, 0.57, 0.67, 1.53);
+@keyframes dots-move-left {
+  0%,65% {left: calc(50% - 115px);}
+  100% {left:35px;}
 }
-
-.dots-item:nth-child(1) .dots-item-button {
-  animation: upDown 1.0s 0.3s alternate;
-  animation-iteration-count: 5;
+.dots__item {
+  width: 36px;
+  height: 36px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: #fff;
+  border-radius: 50%;
 }
-.dots-item:nth-child(2) .dots-item-button {
+.dots__item:nth-child(1) {
+  animation: upDown 1.0s 0.3s infinite alternate;
+  animation-iteration-count: 7;
+}
+.dots__item:nth-child(2) {
   transform: translateX(100px);
 }
-.dots-item:nth-child(3) .dots-item-button {
+.dots__item:nth-child(3) {
   transform: translateX(200px);
-  animation: rightLeft 1.0s 0.3s alternate;
-  animation-iteration-count: 5;
+  animation: rightLeft 1.0s 0.3s infinite alternate;
+  animation-iteration-count: 7;
 }
-
-.dots-item {
-  display: inline-block;
+@keyframes upDown {
+  0% {transform: translateX(0);}
+  100% {transform: translateX(200px);}
 }
-
-.dots-item-button {
-  width: 35px;
-  height: 35px;
-  background-color: #fff;
-  color: #AD4C4C;
+@keyframes rightLeft {
+  0% {transform: translateX(200px);}
+  100% {transform: translateX(0);}
 }
-@-webkit-keyframes upDown {
-  0% {
-    transform: translateX(0);
-  }
-  100% {
-    transform: translateX(200px);
-  }
-}
-
-@-webkit-keyframes rightLeft {
-  0% {
-    transform: translateX(200px);
-  }
-  100% {
-    transform: translateX(0);
-  }
-}
-
-
-
-
 </style>
