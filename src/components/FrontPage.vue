4fff65555e90c9aa78502b954208717355662160
Yuriy Hvozdovych <gvizdyk@quintagroup.com>
2018-08-09 10:57:41 +0300

    up view

--- src/components/FrontPage.vue
+++ src/components/FrontPage.vue
@@ -26,7 +26,22 @@
       <h1>Who we are?</h1>
     </section>
     <section class="slider slider-5">
-      <h1>We are design</h1>
+      <h1>
+        We are
+        <ul class="word-wrap">
+          <li class="word">design</li>
+          <li class="word">web</li>
+          <li class="word">identity</li>
+          <li class="word">graphics</li>
+          <li class="word">front-end</li>
+        </ul>
+        </h1>
+    </section>
+    <section class="slider slider-6">
+      <h1>OGG Team</h1>
+      <p>
+        <a href="mailto:ogg.design.team@gmail.com">ogg.design.team@gmail.com</a>
+      </p>
     </section>
   </div>
 </template>
@@ -50,7 +65,7 @@ export default {
   }
   .slider {
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     flex-wrap: nowrap;
     justify-content: center;
     align-content: center;
@@ -65,6 +80,9 @@ export default {
     right: 0;
     transform: translateX(100%);
     animation-duration: 4s;
+  }
+  .slider a {
+    color: #fff;
   }
   .slider-1 {
     transform: translateX(0);
@@ -132,7 +150,7 @@ export default {
 
   .slider-5 {
     animation-delay: 12.4s;
-    animation-duration: 7s;
+    animation-duration: 8s;
     animation-name: wobble-1;
   }
   @keyframes wobble-1 {
@@ -150,15 +168,20 @@ export default {
 
 
 
-  @keyframes fadeIn1 {
-    0%, 50% {opacity: 0;}
+  .slider-6 {
+    opacity: 0;
+    transform: translateX(0);
+    animation-delay: 18.6s;
+    animation-name: fadeIn-sl6;
+    animation-duration: 7s;
+    animation-fill-mode: forwards;
+  }
+  @keyframes fadeIn-sl6 {
+    0% {opacity: 0;}
     100% {opacity: 1;}
   }
-  @keyframes fadeIn {
-    0% {opacity: 0;}
-    50% {opacity: 1;}
-    100% {opacity: 0;}
-  }
+
+
 
 
 
@@ -173,7 +196,7 @@ export default {
   top: calc(50% - 18px);
   left: calc(50% - 115px);
   animation-duration: 7s, 2s;
-  animation-delay: 0.1s, 16.7s;
+  animation-delay: 0.1s, 17.5s;
   animation-name: dots-move-top, dots-move-left;
   animation-fill-mode: forwards;
 }
@@ -214,4 +237,68 @@ export default {
   0% {transform: translateX(200px);}
   100% {transform: translateX(0);}
 }
+
+
+
+
+
+h1 {
+  display: flex;
+}
+
+
+.word-wrap {
+  list-style: none;
+  margin: 0 0 0 15px;
+  padding: 0;
+  display: inline-block;
+  min-width: 300px;
+}
+.word {
+  position: absolute;
+  opacity: 0;
+  animation-iteration-count: infinite;
+  animation-name: anim;
+  animation-duration: 7.5s;
+  text-decoration: underline;
+}
+.word:nth-child(2) {
+  animation-delay: 1.5s;
+}
+.word:nth-child(3) {
+  animation-delay: 3s;
+}
+.word:nth-child(4) {
+  animation-delay: 4.5s;
+}
+.word:nth-child(5) {
+  animation-delay: 6s;
+}
+@keyframes anim {
+  0% {
+    transform: translateY(100%);
+    opacity: 0;
+  }
+  6.66% {
+    transform: translateY(0);
+    opacity: 1;
+    transition: transform 0.38s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  }
+  20% {
+    transform: translateY(0);
+    opacity: 1;
+    transition: transform 0.38s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  }
+  26.66% {
+    transform: translateY(-100%);
+    opacity: 0;
+    transition: transform 0.32s cubic-bezier(0.55, 0.055, 0.675, 0.19);
+  }
+  100% {
+    transform: translateY(-100%);
+    opacity: 0;
+    transition: transform 0.32s cubic-bezier(0.55, 0.055, 0.675, 0.19);
+  }
+}
+
 </style>
